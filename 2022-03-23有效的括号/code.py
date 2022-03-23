b65f3cbd2e1839f22eef8e633d0894c439799799
kyle <47712424+Kyle-fang@users.noreply.github.com>
2022-03-23 16:23:57 +0800

    Create code.py

---
+++ 2022-03-23有效的括号/code.py
@@ -0,0 +1,20 @@
+def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = list()
+        pairs = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{"
+        }
+        for S in s:
+            if S in pairs:
+                if not stack or stack[-1] != pairs[S]:
+                    return False
+                else:
+                    stack.pop()
+            else:
+                stack.append(S)
+        return not 
